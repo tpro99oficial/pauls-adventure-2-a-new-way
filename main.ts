96d3ad3a1ee845e7d864c59b1649e4e72790e821
Tpro Oficial <77292734+tpro99oficial@users.noreply.github.com>
2021-11-04 20:42:00 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,67 @@
+let paul: Sprite = null
+game.setDialogFrame(img`
+    f f f f f f f f f f f f f f f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f f f f f f f f f f f f f f f 
+    `)
+game.showLongText("Conecting To Pauls Adventure 2: A New Way", DialogLayout.Bottom)
+story.showPlayerChoices("Pauls Adventure 2 Menu", "Play", "Settings", "Multiplayer")
+if (story.checkLastAnswer("Play")) {
+    story.showPlayerChoices("Paul", "Miss Gardlou")
+    if (story.checkLastAnswer("Paul")) {
+        paul = sprites.create(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `, SpriteKind.Player)
+    }
+}
+controller.moveSprite(paul)
+tiles.setTilemap(tilemap`level1`)
+if (story.checkLastAnswer("Miss Gardou")) {
+    paul = sprites.create(img`
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 4 5 5 5 5 4 e f . . . 
+        . . f b 3 e 4 4 4 4 e 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f 3 3 f f e e e e f f 3 3 f . 
+        . f b b f b f e e f b f b b f . 
+        . f b b e 1 f 4 4 f 1 e b b f . 
+        f f b b f 4 4 4 4 4 4 f b b f f 
+        f b b f f f e e e e f f f b b f 
+        . f e e f b d d d d b f e e f . 
+        . . e 4 c d d d d d d c 4 e . . 
+        . . e f b d b d b d b b f e . . 
+        . . . f f 1 d 1 d 1 d f f . . . 
+        . . . . . f f b b f f . . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(paul)
+    tiles.setTilemap(tilemap`level1`)
+}
